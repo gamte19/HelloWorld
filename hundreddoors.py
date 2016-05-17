03c58fc2df991a6b4a41b69df5ea3587215d5a1c
Mate Gonczi <gamte19@gmail.com>
2016-05-17 11:53:32 +0200

    upload hundreddoors.py

--- hundreddoors.py
+++ hundreddoors.py
@@ -1,21 +1,16 @@
-doors = [0] * 101
-
-def switch(x):
-	global doors
-	if doors[x] == 0:
-		doors[x] = 1
-	else :
-		doors[x] = 0
-
+doors =[0] * 101
 for i in range(1,101):
-	for j in range(1,101):
-		if j%i == 0:
-			switch(j)
-			j += 1
-		else:
-			j += 1
-	i += 1
+    for j in range(1,101):
+        if j % i ==0:
+            if doors[j] == 0:
+        		doors[j] = 1
+            else:
+        		doors[j] = 0
+            j+=1
+        else:
+            j+=1
+    i+=1
 
-for y in range (1,101):
-	if doors[y] == 1:
-		print(y)
+for y in range(1,101):
+    if doors[y]==1:
+        print(y)
